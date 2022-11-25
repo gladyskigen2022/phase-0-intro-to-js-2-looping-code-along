d690e4380a64033e665cf0fbedabfaaa1b03224f
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-26 01:22:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(friends,message){
+    let messages = []
+    for (const friend of friends){
+        messages.push(`Thank you, ${friend}, for the wonderful ${message} gift!`)
+    }
+    return messages
+}
+function countDown(n){
+    for(let x=n;x>=0;x--){
+        console.log(x)
+    }
+}
